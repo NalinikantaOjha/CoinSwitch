f878c61336bbcb9e61a5669f9f7970f6f6ead0c6
nalin <nalinikanta7978@gmail.com>
2021-11-01 10:45:19 +0530

    commit from nalini all frame buttom bar navigation all layoutes

--- app/src/main/java/com/masai/nalini/ui/activity/MainActivity.kt
+++ app/src/main/java/com/masai/nalini/ui/activity/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.masai.nalini
+package com.masai.nalini.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.masai.nalini.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this,SignInActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/masai/nalini/ui/fragment/Rewards.kt
@@ -0,0 +1,31 @@
+package com.masai.nalini.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.masai.nalini.R
+
+
+class Rewards : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_rewards, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
